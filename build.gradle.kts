6195ec7a7a7e88795e71af2719eb9dcf4c29edc3
Sam <snewmy8@hotmail.com>
2025-05-30 10:36:48 -0500

    Update to 1.21.5 & add hex support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,14 @@
 plugins {
     java
     `maven-publish`
+    id("xyz.jpenilla.run-paper") version "2.3.0"
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.17"
 }
 
 group = "me.lucyydotp"
-version = "1.1.0"
+version = "1.1.1"
 
-java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+java.toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 
 repositories {
     mavenCentral()
@@ -14,5 +16,12 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
+    paperweight.paperDevBundle("1.21.5-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.5-R0.1-SNAPSHOT")
+}
+
+tasks {
+    runServer {
+        minecraftVersion("1.21.5")
+    }
 }
--- src/main/java/me/lucyy/givepet/GivePet.java
+++ src/main/java/me/lucyy/givepet/GivePet.java
@@ -7,8 +7,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class GivePet extends JavaPlugin {
+
+    public static final Pattern patternBrackets = Pattern.compile("\\{#[0-9a-fA-F]{6}\\}");
+    public static final Pattern pattern = Pattern.compile("#[0-9a-fA-F]{6}");
 
     private final Map<UUID, TransferAttempt> transferAttempts = new HashMap<>();
 
@@ -20,8 +25,30 @@ public final class GivePet extends JavaPlugin {
         return transferAttempts.remove(uuid) != null;
     }
 
+    public static String colorMsg(String s) {
+        s = removeBrackets(s);
+        Matcher match = pattern.matcher(s);
+        while (match.find()) {
+            String color = s.substring(match.start(), match.end());
+            s = s.replace(color, "" + net.md_5.bungee.api.ChatColor.of(color));
+            match = pattern.matcher(s);
+        }
+        return net.md_5.bungee.api.ChatColor.translateAlternateColorCodes('&', s);
+    }
+
+    private static String removeBrackets(String text) {
+        Matcher m = patternBrackets.matcher(text);
+        String replaced = text;
+        while (m.find()) {
+            String hexcode = m.group();
+            String fixed = hexcode.substring(2, 8);
+            replaced = replaced.replace(hexcode, "#" + fixed);
+        }
+        return replaced;
+    }
+
     public String getMsg(String key) {
-        return ChatColor.translateAlternateColorCodes('&', getConfig().getString(key));
+        return colorMsg(getConfig().getString(key));
     }
 
     @Override
